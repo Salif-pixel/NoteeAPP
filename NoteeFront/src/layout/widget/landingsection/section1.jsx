5bf21c9c2bfb4244b76c5780dd9aa0e1d71e6649
Salif Biaye <salifbiaye@esp.sn>
2024-06-05 20:01:43 +0000

    first commit

--- NoteeFront/src/layout/widget/landingsection/section1.jsx
+++ NoteeFront/src/layout/widget/landingsection/section1.jsx
@@ -20,10 +20,10 @@ return (
                                 className="flex backdrop-blur-lg flex-auto justify-center w-screen h-screen  items-center flex-col gap-4  ">
                         <motion.div initial={{x: -300}} whileInView={{x: 0}}
                                     transition={{type: "spring", stiffness: 100, damping: 25, duration: 2}}
-                                    className={`w-52 h-52 bg-${colortheme}-500 opacity-100 rounded-full absolute top-40 right-20`}></motion.div>
-                        <motion.div initial={{x: 300}} whileInView={{x: 0}}
+                                    className={`w-42 h-42 bg-${colortheme}-500 opacity-100 rounded-full absolute top-40 right-20`}></motion.div>
+                        <motion.div initial={{x: 200}} whileInView={{x: 0}}
                                     transition={{type: "spring", stiffness: 100, damping: 25, duration: 2}}
-                                    className={`w-32 h-32 bg-${colortheme}-500 opacity-100 rounded-full absolute bottom-24 left-20`}></motion.div>
+                                    className={`w-32 h-32 bg-${colortheme}-500 opacity-100 rounded-full absolute bottom-40 left-20`}></motion.div>
                         <div
                             className={`flex flex-col justify-center w-screen  absolute left-0 top-0 right-0 bottom-0 backdrop-blur-lg  items-center`}>
                             <div className={`flex flex-col justify-center w-screen items-center`}>
