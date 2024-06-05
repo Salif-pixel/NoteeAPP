a977aafe761ad16a8110a330d7cfec531c338040
Salif Biaye <salifbiaye@esp.sn>
2024-06-05 19:07:47 +0000

    first commit

--- NoteeBackend/src/app.module.ts
+++ NoteeBackend/src/app.module.ts
@@ -7,12 +7,13 @@ import { SocketModule } from './Socket/SocketModule';
 import { AppGateway } from './app.gatway';
 import { NoteModule } from './note/note.module';
 import { CategorieModule } from './categorie/categorie.module';
+import {AppController} from "./app.controller";
 
 @Module({
   imports: [UserModule,SocketModule, AuthModule,SocketModule,ConfigModule.forRoot({
     isGlobal: true,
   }), NoteModule, CategorieModule],
-  controllers: [],
+  controllers: [AppController],
   providers: [AppGateway],
 })
 export class AppModule {}
